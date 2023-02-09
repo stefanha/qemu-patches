c97399119b76596380af6bd57b8123b7c4113508
Stefan Hajnoczi <stefanha@gmail.com>
2023-02-09 11:28:23 -0500

    run.sh: use GitLab git repo URL
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ ! -d /home/patches/patches ]; then
 	/bin/su - patches -c 'git clone https://github.com/stefanha/patches.git'
 fi
 if [ ! -d /home/patches/qemu.git ]; then
-	/bin/su - patches -c 'git clone --mirror git://git.qemu-project.org/qemu.git'
+	/bin/su - patches -c 'git clone --mirror https://gitlab.com/qemu-project/qemu.git'
 fi
 
 exec /usr/sbin/crond -n
